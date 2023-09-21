d26748163166926ebc5951665058a435768c5f7d
Alfian Vito Anggoro <alfianvitoanggoro@gmail.com>
2023-09-21 12:15:52 +0700

    second commit to add all

---
+++ components/NavLink/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function NavLink({ href, children }) {
+  // console.log(useRouter());
+  const router = useRouter();
+  return (
+    <Link href={href}>
+      <span className={`nav-link  ${router.pathname === href ? 'active' : ''}`} aria-current="page">
+        {children}
+      </span>
+    </Link>
+  );
+}
